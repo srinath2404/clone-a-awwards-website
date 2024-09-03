8f8b905eb1861aa414a36278bd6b6827122369b4
srinath2404 <143181201+srinath2404@users.noreply.github.com>
2024-09-03 22:31:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
+// function firstpageAni(){
+//     var a1= gsap.timeline();
+
+//     a1.from("#nav-top",{
+//         y:'-10',
+//         opacity:0,
+//         duration:1.5,
+//         ease:Expo.easeInout
+//     });
+//     a1.to(".boundelm ",{
+//         y:'0',
+//         duration:2,
+//         ease:Expo.easeInout,
+//         stagger:.2
+//     });
+
+// }
+// function circlemousefollow(){
+//     window.addEventListener("mousemove",function(){
+//         document.querySelector("#minicircle").style.transform =`translate(${dets.clientX}px, ${dets.clientY}px)`
+//     })
+// }
+// firstpageAni();
+// circlemousefollow();
